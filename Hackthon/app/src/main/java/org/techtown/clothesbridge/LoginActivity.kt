3ac6ba27ad01e77d2780ffd850eba26b9ffb0211
jiwon <jjwon4086@gmail.com>
2023-02-12 10:50:51 +0900

    add Google login

--- Hackthon/app/src/main/java/org/techtown/clothesbridge/LoginActivity.kt
+++ Hackthon/app/src/main/java/org/techtown/clothesbridge/LoginActivity.kt
@@ -1,29 +1,89 @@
 package org.techtown.clothesbridge
 
+import android.util.Log
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View.inflate
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    //var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
         val loginbtn = findViewById<Button>(R.id.loginbtn)
 
-        loginbtn.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        loginbtn.setOnClickListener() {
+            // first step
+            googleLogin()
         }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+    }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result -> if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            var signinTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = signinTask.getResult(ApiException::class.java)
+                Log.d("LOG", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account)
+            }
+            catch(e: ApiException) {
+                Log.w("LOG", "Google sign in failed", e)
+            }
+        } else {
+            Log.e("LOG", "Google Result Error")
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(){
+                task ->
+                if(task.isSuccessful){
+                    //Login
+                    moveHomePage(task.result?.user)
+                } else {
+                    //Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun moveHomePage(user:FirebaseUser?){
+        if (user != null)
+            startActivity(Intent(this,MainActivity::class.java))
     }
 }
